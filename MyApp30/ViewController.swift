dff34095f51bd1054398cd928730c95e3e7f4903
Brad <brad@brad.tw>
2017-10-05 09:41:02 +0800

    2017100501

--- MyApp30/ViewController.swift
+++ MyApp30/ViewController.swift
@@ -12,8 +12,12 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
+    var recorder:AVAudioRecorder? = nil
+    var player:AVAudioPlayer? = nil
+    
+
     @IBAction func recorder(_ sender: Any) {
-        let file = NSHomeDirectory() + "/Documents/audio.mp3"
+        let file = NSHomeDirectory() + "/Documents/audio.ima4"
         let url = URL(fileURLWithPath: file)
         
         let session = AVAudioSession.sharedInstance()
@@ -21,20 +25,53 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDe
             try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
             try session.setActive(true)
             
-        
+            // 組態 [String:NSNumber]
+            let config = [
+                AVFormatIDKey : NSNumber(value: kAudioFormatAppleIMA4),
+                AVEncoderAudioQualityKey : NSNumber(value: AVAudioQuality.high.rawValue),
+                AVEncoderBitRateKey : NSNumber(value: 16),
+                AVNumberOfChannelsKey : NSNumber(value: 2),
+                AVSampleRateKey : NSNumber(value: 44100)
+            ]
             
-            
+            do {
+                try recorder = AVAudioRecorder(url: url, settings: config)
+                recorder?.delegate = self
+                
+                if (recorder?.record(forDuration: 10))! {
+                    recorder?.record()
+                    print(NSHomeDirectory())
+                    print("開始錄音")
+                }else{
+                    print("error 1")
+                }
+                
+//                if (recorder?.record())! {
+//                    print("開始錄音")
+//                }else{
+//                    print("錄音error")
+//                }
+                
+//                if let _ = recorder {
+//                    if recorder?.isRecording {
+//                        recorder?.stop()
+//                    }
+//                }
+                
+            } catch {
+                print("error 2 : \(error)")
+            }
         }catch{
-            
+            print("session error: \(error)")
         }
         
         
     }
     
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("錄音結束:\(flag)")
+    }
     
-    var recorder:AVAudioRecorder? = nil
-    var player:AVAudioPlayer? = nil
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
